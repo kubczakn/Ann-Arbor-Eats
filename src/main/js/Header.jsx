990831376c24ae4a95cf34e656f42bd7920c1def
kubczakn <kubczakn@umich.edu>
2021-06-03 17:02:08 -0400

    redesigned create dialog

--- src/main/js/Header.jsx
+++ src/main/js/Header.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Create from './Modals/Create.jsx'
 import {AppBar, Button, Grid, Toolbar, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
@@ -14,65 +15,12 @@ const useStyles = makeStyles({
 // TODO: Allow for edits on individual review attributes; probably need to add patch endpoint
 const Header = ( {attributes, onCreate}) => {
     const classes = useStyles();
-    const [name, setName] = useState(null);
-    const [rating, setRating] = useState(null);
-    const [description, setDescription] = useState(null);
-    const [image, setImage] = useState(null);
 
-    const handleUpload = (e) => {
-        setImage(e.target.files[0]);
-    }
-
-    // TODO: Figure out cleaner way of handling form
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('name', name);
-        formData.append('rating', rating);
-        formData.append('description', description);
-        formData.append('image', image);
-        onCreate(formData);
-
-        // Clear out the dialog's inputs
-        setName(null);
-        setRating(null);
-        setDescription(null);
-        setImage(null);
-
-        // Navigate away from the dialog to hide it.
-        window.location = "#";
-	}
-
-	// TODO: Extract create dialog
     return (
         <Grid item className={classes.root} xs={12}>
             <AppBar position={'static'}>
                 <Toolbar>
-                    <Button href="#createPost">
-                        <Typography className={classes.text}>Create</Typography>
-                    </Button>
-                    <div id="createPost" className="modalDialog">
-                        <div>
-                            <a href="#" title="Close" className="close">X</a>
-                            <Typography>Create New Post</Typography>
-                            {/*<h2>Create new post</h2>*/}
-                            <form  encType="multipart/form-data">
-                                <p>
-                                    <input type="text" placeholder="name" className="field" onChange={event => setName(event.target.value)}/>
-                                </p>
-                                <p>
-                                    <input type="text" placeholder="rating"  className="field" onChange={event => setRating(event.target.value)}/>
-                                </p>
-                                <p>
-                                    <input type="text" placeholder="description"  className="field" onChange={event => setDescription(event.target.value)}/>
-                                </p>
-                                <p>
-                                    <input type="file" name="image" accept={"image/png, image/jpeg"} className="field" onChange={handleUpload}/>
-                                </p>
-                                <button onClick={handleSubmit}>Create</button>
-                            </form>
-                        </div>
-                    </div>
+                    <Create attributes={attributes} onCreate={onCreate}/>
                 </Toolbar>
             </AppBar>
         </Grid>
@@ -80,21 +28,3 @@ const Header = ( {attributes, onCreate}) => {
 }
 
 export default Header;
-
-
-// handleSubmit(e) {
-//     e.preventDefault();
-//     const formData = new FormData();
-//     this.props.attributes.forEach(attribute => {
-//         formData.append(attribute, ReactDOM.findDOMNode(this.refs[attribute]).value.trim());
-//     });
-//     this.props.onCreate(formData);
-//
-//     // clear out the dialog's inputs
-//     this.props.attributes.forEach(attribute => {
-//         ReactDOM.findDOMNode(this.refs[attribute]).value = '';
-//     });
-//
-//     // Navigate away from the dialog to hide it.
-//     window.location = "#";
-// }
--- src/main/js/Modals/Create.jsx
+++ src/main/js/Modals/Create.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {makeStyles} from "@material-ui/core/styles";
+import {
+    Button,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+     TextField,
+    Typography
+} from "@material-ui/core";
 
-const CreateDialog = () => {
-    return(
-        <div>Fixme</div>
+const useStyles = makeStyles({
+    text: {
+        color: '#ffffff',
+    }
+});
+
+// TODO: Make prettier
+const CreateDialog = ( {attributes, onCreate}) => {
+    const classes = useStyles();
+    const [name, setName] = useState(null);
+    const [rating, setRating] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [image, setImage] = useState(null);
+    const [open, setOpen] = useState(false);
+
+    const handleUpload = (e) => {
+        setImage(e.target.files[0]);
+    }
+
+    // TODO: Figure out cleaner way of handling form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('rating', rating);
+        formData.append('description', description);
+        formData.append('image', image);
+        onCreate(formData);
+
+        // Clear out the dialog's inputs
+        setName(null);
+        setRating(null);
+        setDescription(null);
+        setImage(null);
+
+        // Exit from window
+        setOpen(false);
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button onClick={handleClickOpen}>
+                <Typography className={classes.text}>Create</Typography>
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Create a new post</DialogTitle>
+                <DialogContent>
+                    <form encType="multipart/form-data">
+                        <TextField
+                            label="Name"
+                            type="text"
+                            onChange={event => setName(event.target.value)}
+                        />
+                        <TextField
+                            label="Rating"
+                            type="text"
+                            onChange={event => setRating(event.target.value)}
+                        />
+                        <TextField
+                            label="Description"
+                            type="text"
+                            onChange={event => setDescription(event.target.value)}
+                        />
+                        <TextField
+                            label="Image"
+                            type="file"
+                            accept="image/png, image/jpeg"
+                            onChange={handleUpload}
+                        />
+                        <Button onClick={handleSubmit}>Create</Button>
+                    </form>
+                </DialogContent>
+            </Dialog>
+
+        </div>
     );
 }
 
 export default CreateDialog;
+
